ecce16c41753e499257ece77fd102da4a6a39a52
nansmatty <narayanmaitysp1997@gmail.com>
2022-07-05 13:03:56 +0530

    shop page starts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
-				<Route path='/auth' element={<Authentication />} />
+				<Route path='shop' element={<Shop />} />
+				<Route path='auth' element={<Authentication />} />
 			</Route>
 		</Routes>
 	);
