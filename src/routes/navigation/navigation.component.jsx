9748b667a3b3a84e96fc7222b2c274a669135767
nansmatty <narayanmaitysp1997@gmail.com>
2022-07-05 19:35:28 +0530

    cart icon and dropdown design complete

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
@@ -27,7 +29,9 @@ const Navigation = () => {
 							SIGNIN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
+				<CartDropdown />
 			</div>
 			<Outlet />
 		</Fragment>
