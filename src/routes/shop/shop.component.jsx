0059206811d16f8ddbdcdd394d90989305fdeab5
nansmatty <narayanmaitysp1997@gmail.com>
2022-07-09 17:29:56 +0530

    category preview component created

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from 'react';
-import ProductCard from '../../components/product-card/product-card.component';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 import { CategoryContext } from '../../contexts/categories.context';
 import './shop.styles.scss';
 
@@ -7,18 +7,14 @@ const Shop = () => {
 	const { categoriesMap } = useContext(CategoryContext);
 
 	return (
-		<>
-			{Object.keys(categoriesMap).map((title) => (
-				<Fragment key={title}>
-					<h2>{title}</h2>
-					<div className='products-container'>
-						{categoriesMap[title].map((product) => (
-							<ProductCard key={product.id} product={product}></ProductCard>
-						))}
-					</div>
-				</Fragment>
-			))}
-		</>
+		<div className='shop-container'>
+			{Object.keys(categoriesMap).map((title) => {
+				const products = categoriesMap[title];
+				return (
+					<CategoryPreview key={title} title={title} products={products} />
+				);
+			})}
+		</div>
 	);
 };
 
