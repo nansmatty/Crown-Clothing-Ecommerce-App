54b805b7beba964b0892f0836fe9c7e6c7a8adf6
nansmatty <narayanmaitysp1997@gmail.com>
2022-07-14 18:32:28 +0530

    redux persist added in library and used inside the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
+// import logger from 'redux-logger';
 import {
 	compose,
 	legacy_createStore as createStore,
 	applyMiddleware,
 } from 'redux';
-// import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { rootReducer } from './root-reducer';
 
 const loggerMiddlware = (store) => (next) => (action) => {
@@ -20,8 +23,18 @@ const loggerMiddlware = (store) => (next) => (action) => {
 	console.log('next state: ', store.getState());
 };
 
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [loggerMiddlware];
 
 const composeEnhancer = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composeEnhancer);
+export const store = createStore(persistedReducer, undefined, composeEnhancer);
+
+export const persistor = persistStore(store);
