21b814551a7775c91ca6edb81792379b2895d3ff
nansmatty <narayanmaitysp1997@gmail.com>
2022-07-23 19:22:05 +0530

    cart selector and reducer completely changed into typescript

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,34 @@
+import { CartItem } from './cart.types';
+import { AnyAction } from 'redux';
+import { setCartItems, setIsCartOpen } from './cart.actions';
+
+export type CartState = {
+	readonly isCartOpen: boolean;
+	readonly cartItems: CartItem[];
+};
+
+export const CART_INITIAL_STATE: CartState = {
+	isCartOpen: false,
+	cartItems: [],
+};
+
+export const cartReducer = (
+	state = CART_INITIAL_STATE,
+	action: AnyAction
+): CartState => {
+	if (setIsCartOpen.match(action)) {
+		return {
+			...state,
+			isCartOpen: action.payload,
+		};
+	}
+
+	if (setCartItems.match(action)) {
+		return {
+			...state,
+			cartItems: action.payload,
+		};
+	}
+
+	return state;
+};
