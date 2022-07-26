53ff00afc67815691cb9f43380d2cec7fc82db9e
nansmatty <narayanmaitysp1997@gmail.com>
2022-07-26 21:09:59 +0530

    button component convereted into typescript extended or tsx file

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,3 +1,4 @@
+import { ButtonHTMLAttributes } from 'react';
 import {
 	BaseButton,
 	GoogleSignInButton,
@@ -5,20 +6,30 @@ import {
 	ButtonSpinner,
 } from './button.styles';
 
-export const BUTTON_TYPES_CLASSES = {
-	base: 'base',
-	google: 'google-sign-in',
-	inverted: 'inverted',
-};
+export enum BUTTON_TYPES_CLASSES {
+	base = 'base',
+	google = 'google-sign-in',
+	inverted = 'inverted',
+}
 
-const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base): typeof BaseButton =>
 	({
 		[BUTTON_TYPES_CLASSES.base]: BaseButton,
 		[BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
 		[BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
 	}[buttonType]);
 
-const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+export type ButtonProps = {
+	buttonType?: BUTTON_TYPES_CLASSES;
+	isLoading?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({
+	children,
+	buttonType,
+	isLoading,
+	...otherProps
+}) => {
 	const CustomButton = getButton(buttonType);
 
 	return (
