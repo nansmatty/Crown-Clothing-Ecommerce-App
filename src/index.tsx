d77707344b15b0b84dad742fa63ba290ac73d7c2
nansmatty <narayanmaitysp1997@gmail.com>
2022-08-01 19:10:32 +0530

    manually added serviceWorker.js file

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { persistor, store } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -30,3 +31,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
